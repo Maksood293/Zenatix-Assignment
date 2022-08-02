14ee505eee122f105eec2077af29e289fe0c5b42
Maksood293 <maksoodkhan0143@gmail.com>
2022-08-03 02:24:22 +0530

    Previous and next button implemented

--- src/App.js
+++ src/App.js
@@ -1,100 +1,112 @@
-import logo from './pokedex.2800773d.png';
-import './App.css';
-import Input from './components/common/Input/Input';
-import Dropdown from './components/common/Dropdown/Dropdown';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Card from './components/common/Card/card'
+import logo from "./pokedex.2800773d.png";
+import "./App.css";
+import Input from "./components/common/Input/Input";
+import Dropdown from "./components/common/Dropdown/Dropdown";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/common/Card/card";
 
 function App() {
   const [searchData, setSearchData] = useState();
-  const [data, setData]=useState();
-  const [modal, setModal]= useState()
-  const [details,setDetails]=useState()
-  const [type, setType]=useState()
+  const [data, setData] = useState();
+  const [modal, setModal] = useState();
+  const [details, setDetails] = useState();
+  const [type, setType] = useState();
+  const [offset, setOffset] = useState(0);
 
-  useEffect(()=>{
-      getAllPokemons(0, 20);
-  },[])
+  useEffect(() => {
+    getAllPokemons(offset, 20);
+  }, [offset]);
 
-  
-  useEffect(()=>{
-if(type && type !== 'Select Type'){
-  const filterData = data.filter((item)=>
-         item.types[0].type.name ===type
-  )
-  setData(filterData)
-}
-  },[type])
-
-  
-
- 
-  
-const getPokemonData = async (result) => {
-  const pokemonArr = []
-
-  await Promise.all(
-      result.map((pokemonItem) => {
-          return axios
-              .get(`https://pokeapi.co/api/v2/pokemon/${pokemonItem.name}`)
-              .then((result) => {
-                  pokemonArr.push(result.data);
-              });
-      })
-  );
-
-  pokemonArr.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
-
-  setData(pokemonArr)
-
-  
-
-}
-const getAllPokemons = async (offset, limit) => {
-  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).catch((err) => console.log("Error:", err));
-  getPokemonData(response.data.results);
-
-}
-
-
-const handleModal=(data)=>{
-   setModal(data)
-}
-
-const handleDetails=(itemDetails)=>{
-  setDetails(itemDetails)
-}
-
-const handleDropdownChange=(e)=>{
-  console.log(e.target.value)
-  setType(e.target.value)
-}
-
-const handleSearch=(e)=>{
-  setSearchData(e.target.value)
-}
-
-const search=(row)=> {
-  return row && row.filter(
-        (el) =>
-          el.name.toLowerCase().indexOf(searchData.toLowerCase()) > -1 
-      )
+  useEffect(() => {
+    if (type && type !== "Select Type") {
+      const filterData = data.filter(
+        (item) => item.types[0].type.name === type
+      );
+      setData(filterData);
     }
-console.log("type",type)
+  }, [type]);
+
+  const getPokemonData = async (result) => {
+    const pokemonArr = [];
+
+    await Promise.all(
+      result.map((pokemonItem) => {
+        return axios
+          .get(`https://pokeapi.co/api/v2/pokemon/${pokemonItem.name}`)
+          .then((result) => {
+            pokemonArr.push(result.data);
+          });
+      })
+    );
+
+    pokemonArr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+
+    setData(pokemonArr);
+  };
+  const getAllPokemons = async (offset, limit) => {
+    const response = await axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+      .catch((err) => console.log("Error:", err));
+    getPokemonData(response.data.results);
+  };
+
+  const handleModal = (data) => {
+    setModal(data);
+  };
+
+  const handleDetails = (itemDetails) => {
+    setDetails(itemDetails);
+  };
+
+  const handleDropdownChange = (e) => {
+    console.log(e.target.value);
+    setType(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    setSearchData(e.target.value);
+  };
+
+  const search = (row) => {
+    return (
+      row &&
+      row.filter(
+        (el) => el.name.toLowerCase().indexOf(searchData.toLowerCase()) > -1
+      )
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="header-logo" alt="logo" />
-        <div className='header-inputs'>
-          <Input handleChange={handleSearch} type="text" label="Search" placeholder="Search By Name" />
-          <Dropdown handleChange={handleDropdownChange} labelName="Type"/>
+        <div className="header-inputs">
+          <Input
+            handleChange={handleSearch}
+            type="text"
+            label="Search"
+            placeholder="Search By Name"
+          />
+          <Dropdown handleChange={handleDropdownChange} labelName="Type" />
         </div>
-        
       </header>
       <section>
-      {data && <Card data={searchData? search(data):data} handleModal={handleModal} modal={modal} handleDetails={handleDetails} details={details}/>}
+        {data && (
+          <Card
+            data={searchData ? search(data) : data}
+            handleModal={handleModal}
+            modal={modal}
+            handleDetails={handleDetails}
+            details={details}
+          />
+        )}
       </section>
+      <footer className="App-footer">
+        <button onClick={() => (offset > 0 ? setOffset(offset - 20) : null)}>
+          Previous
+        </button>
+        <button onClick={() => setOffset(offset + 20)}>Next</button>
+      </footer>
     </div>
   );
 }
